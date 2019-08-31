36df327e0cfff6f7ec5a8447cd318f00fc44bec9
magomedovgh@gmail.com <magomedovgh@gmail.com>
2019-08-31 21:28:22 +0300

    refactored IP constructor

--- src/main/java/IP.java
+++ src/main/java/IP.java
@@ -1,38 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-
-public class IP implements Comparable<IP>{
+public class IP implements Comparable<IP> {
 
     private int octet1;
     private int octet2;
     private int octet3;
     private int octet4;
 
-    IP (String ip) throws Exception {
+    IP(String ip) throws Exception {
         String[] octets = ip.split("\\.");
 
         if (octets.length != 4)
             throw new Exception("Invalid ip: " + ip);
 
 
+        List<Integer> intOctets;
+
         try {
-            octet1 = Integer.parseInt(octets[0]);
-            octet2 = Integer.parseInt(octets[1]);
-            octet3 = Integer.parseInt(octets[2]);
-            octet4 = Integer.parseInt(octets[3]);
+            intOctets = Arrays.stream(octets)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
         } catch (NumberFormatException e) {
             throw new Exception("some octets are non-numeric: " + ip);
         }
 
+        for (Integer octet : intOctets) {
+            if (octet < 0 || octet > 255)
+                throw new Exception("some octets are less than 0 or higher than 255: " + ip);
+        }
 
-        if (octet1 < 0 || octet1 > 255 ||
-            octet2 < 0 || octet2 > 255 ||
-            octet3 < 0 || octet3 > 255 ||
-            octet4 < 0 || octet4 > 255)
-            throw new Exception("some octets are less than 0 or higher than 255: " + ip);
-
+        octet1 = intOctets.get(0);
+        octet2 = intOctets.get(1);
+        octet3 = intOctets.get(2);
+        octet4 = intOctets.get(3);
     }
 
-    private IP(int octet1,  int octet2, int octet3, int octet4) {
+    private IP(int octet1, int octet2, int octet3, int octet4) {
         this.octet1 = octet1;
         this.octet2 = octet2;
         this.octet3 = octet3;
