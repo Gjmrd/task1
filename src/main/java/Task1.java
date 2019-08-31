c1c6af66add2c9f946958bc18829b5ab750eac62
magomedovgh@gmail.com <magomedovgh@gmail.com>
2019-08-31 09:13:44 +0300

    handled exception when parsing octets of ip address

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,6 +1,6 @@
 public class Task1 {
-    public static void main (String[] args){
-        if (args.length > 2) {
+    public static void main(String[] args) {
+        if (args.length >= 2) {
             try {
                 final long from = getLongFromIp(args[0]);
                 final long to = getLongFromIp(args[1]);
@@ -10,18 +10,18 @@ public class Task1 {
                         System.out.println(getIPFromLong(longIp));
                     }
                 }
+            } catch (Exception e) {
+                System.out.println("something is wrong: " + e.getMessage());
             }
-            catch (Exception e) {
-                System.out.println("Something is wrong: " + e.getMessage());
-            }
-        }
-        else {
+
+        } else {
             System.out.println("invalid arguments");
         }
     }
 
     /**
      * Converts integer representation of ip address into string
+     *
      * @param ipLong long int representation of ip address
      * @return string representation of ip address
      */
@@ -29,12 +29,11 @@ public class Task1 {
         return String.format("%d.%d.%d.%d",
                 (ipLong >>> 24) & 0xff,
                 (ipLong >>> 16) & 0xff,
-                (ipLong >>>  8) & 0xff,
-                (ipLong       ) & 0xff);
+                (ipLong >>> 8) & 0xff,
+                (ipLong) & 0xff);
     }
 
     /**
-     *
      * @param ip string representation of ip address
      * @return long int representation of ip address
      * @throws Exception if argument is invalid
@@ -44,14 +43,20 @@ public class Task1 {
 
         String[] parts = ip.split("\\.");
         if (parts.length != 4) {
-            throw new Exception("invalid ip");
+            throw new Exception("invalid ip: " + ip);
         }
 
         int intPart = 0;
         for (int i = 0; i < 4; i++) {
-            intPart = Integer.parseInt(parts[i]);
+
+            try {
+                intPart = Integer.parseInt(parts[i]);
+            } catch (NumberFormatException e) {
+                throw new Exception("invalid ip: " + ip);
+            }
+
             if (intPart < 0 || intPart > 255) {
-                throw new Exception("invalid ip");
+                throw new Exception("invalid ip: " + ip);
             }
             result += intPart * Math.pow(256, 3 - i);
         }
--- src/test/java/Task1Test.java
+++ src/test/java/Task1Test.java
@@ -35,28 +35,35 @@ public class Task1Test {
     }
 
     @Test
-    public void testInvalidIpException() throws Exception{
+    public void invalidIpException() throws Exception{
         expectedException.expect(Exception.class);
-        expectedException.expectMessage("invalid ip");
+        expectedException.expectMessage("invalid ip: 192.168.0" );
         Task1.getLongFromIp("192.168.0");
     }
 
     @Test
-    public void testInvalidIpPartException() throws Exception {
+    public void invalidIpPartException() throws Exception {
         expectedException.expect(Exception.class);
-        expectedException.expectMessage("invalid ip");
+        expectedException.expectMessage("invalid ip: 192.256.0.1");
         Task1.getLongFromIp("192.256.0.1");
     }
 
     @Test
-    public void main() {
+    public void nonNumericIpPartException() throws Exception {
+        expectedException.expect(Exception.class);
+        expectedException.expectMessage("invalid ip: 192.256.absc.1");
+        Task1.getLongFromIp("192.256.absc.1");
+    }
+
+    @Test
+    public void main() throws Exception {
         String[] arguments = new String[]{"192.168.0.1", "192.168.0.5"};
         Task1.main(arguments);
         Assert.assertEquals("192.168.0.2\r\n192.168.0.3\r\n192.168.0.4\r\n", outContent.toString());
     }
 
     @Test
-    public void mainWithWrongArgs() {
+    public void mainWithWrongArgs() throws Exception {
         String[] arguments = new String[]{"192.168.0.1"};
         Task1.main(arguments);
         Assert.assertEquals("invalid arguments\r\n", outContent.toString());
